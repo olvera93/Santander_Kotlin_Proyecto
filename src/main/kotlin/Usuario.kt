a863b355849200b2a8609c13ff34a20919bb98c7
Olvera93 <olvera9302@outlook.com>
2021-06-24 11:31:16 -0500

    Se arregló el registro del usuario

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -4,66 +4,70 @@ class Usuario {
 
     private var usuario: String = ""
     private var password: String = ""
-
     private var coorActuales: Int = 0
     private var coorDestino: Int = 0
-
     private var tipoPago: String = ""
-        fun setTipoPago(pago: String){  //Utilizamos un setter para guardar un valor para el pago ya sea Efectivo o Tarjeta
-            if(pago == "Efectivo" || pago == "Tarjeta"){
-                this.tipoPago = pago
-            }
-        }
-
-
+        set(value) {
+            field=value
+            when(field) {
+                "Tarjeta" -> {
+                    field=value
+                }
+                "Efectivo" -> {
+                    field=value
+                }
+                else -> {
+                    println("Método de pago incorrecto $tipoPago")
+                }
+            }}
+    private var Estatus: String=""
     fun getTipoPago(): String {
         return tipoPago
     }
 
+
     fun getUsuario(): String {
         return usuario
     }
-
     fun setUsuario(usuario: String) {
-        this.usuario = usuario
+        if(usuario.isEmpty()){
+            println("No ingreso un dato valido")
+        }else {
+            this.usuario = usuario
+        }
     }
     fun getPassword(): String {
         return password
     }
-
     fun setPassword(password: String) {
         this.password = password
     }
-
     fun getCoorActuales(): Int {
         return coorActuales
     }
-
     fun setCoorActuales(coordenadaActual: Int) {
         this.coorActuales = coorActuales
     }
-
     fun getCoorDestino(): Int {
         return coorDestino
     }
-
     fun setCoorDestino(coordenadaDestino: Int) {
         this.coorDestino = coorDestino
     }
 
-
-
-
     //Funcion para el inicio de sesion del usuario
     fun loginUsuario(user: String, password: String): Boolean{
-
         fun validate(input: String, input2: String):Boolean {
             if (input.isEmpty() || input2.isEmpty()) {
                 println("Falta un parametro")
                 return false
-            }
-            return true
-     }
+            }else if(input==this.usuario && input2==password){
+                println("Bienvenido $usuario")
+                return true}
+            else
+                println("Usuario o contraseña incorrectos")
+            return false
+        }
         val userValidate = validate(user,password)
         return userValidate
     }
@@ -77,10 +81,8 @@ class Usuario {
             }
             return true
         }
-
         val coorActualValidate = validate(coordenadaActual)
         val coorDestinoValidate = validate(coordenadaDestino)
-
         return coorActualValidate && coorDestinoValidate
     }
 
@@ -94,12 +96,17 @@ class Usuario {
             return true
         }
         val pagoValidate = validate(pago)
-
         return pagoValidate
     }
 
-
-
-
-
+    // Funcion para el registro de un usuario
+    fun RegistroUsuario(usuario: String,password: String, pago: String){ //Comprobacion que en el registro no tenga valores invalidos
+        if(usuario.isEmpty()||password.isEmpty()||pago.isEmpty()){
+            println("No ingreso un dato valido")
+        }else{
+            this.usuario=usuario
+            this.password=password
+            this.tipoPago=pago
+            println("Registro exitoso")}
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,59 @@
 
 package Proyecto
-
 import java.util.*
 import kotlin.system.exitProcess
-
 const val PRECIO_BASE = 30
 const val PRECIO_POR_COR : Int = 5
-
 fun main() {
 
+    val usuario = Usuario()
+
     var Salidado = false
+    var pago:String = ""
+
     //Menu Inicio de sesion, Registrar usuario y salir de la aplicacion
     do {
         println()
         println("Bienvenido a Carpool.")
         println("Por favor ingrese una opcion para utilizar nuestra aplicacion")
-        println("1.- Iniciar Sesion")
-        println("2.- Registrarse")
+        println("1.- Registrarse")
+        println("2.- Iniciar Sesion")
         println("3.- Salir de la aplicacion")
         var opcionMenu= readLine()?.toInt()
         when (opcionMenu){
             1 -> {
+                println()
+                println("Usted selecciono Registro")
+                print("Ingresa tu nombre: ")
+                val nombreUsuario = readLine().toString()
+                print("Ingresa tu contraseña: ")
+                val passwordUsuario = readLine().toString()
+                do{
+                    println("Ingresa tu forma de pago: Efectivo o Tarjeta: ")
+                    pago = readLine()!!.toString()
+                    when(pago) {
+                        "Efectivo" -> {
+                            println("Agregado Exitosamente el pago con efectivo")
+                        break
+                    }
+                        "Tarjeta" -> {
+                            println("Agregado Exitosamente el pago con tarjeta")
+                        break
+                        }
+
+                        else -> {
+                            println()
+                            println("METODO DE PAGO INCORRECTO")
+                        }
+                    }
+                } while (pago.isEmpty() || pago != "Efectivo" || pago != "Tarjeta")
+                usuario.setUsuario(nombreUsuario)
+                usuario.setPassword(passwordUsuario)
+                usuario.RegistroUsuario(usuario.getUsuario(),usuario.getPassword(),pago)
+
+            }
+
+            2 -> {
                 println()
                 println("Usted selecciono Iniciar Sesion")
                 //login de usuario
@@ -29,155 +62,74 @@ fun main() {
                 print("Ingresa tu contraseña: ")
                 val passwordUsuario = readLine().toString()
 
-                val usuario = Usuario()
-                usuario.setUsuario(nombreUsuario)
-                usuario.setPassword(passwordUsuario)
-                usuario.loginUsuario(nombreUsuario, passwordUsuario)
-
-
-
-                if (!usuario.getUsuario().equals("") && !usuario.getPassword().equals("")){
-                    println()
-                    /*
-                    El usuario después de iniciar sesion tiene que ingresar sus coordenadas
-                    Coordenadas actuales en donde está ubicado actualmente
-                    Coordenadas destino a donde quiere ir
-                     */
-                    print("Ingrese sus coordenadas actuales: ")
-                    var coordenadasActuales = readLine()!!.toInt()
-                    print("Ingrese las coordenadas de su destino: ")
-                    var coordenadaDestino = readLine()!!.toInt()
-
-                    usuario.setCoorActuales(coordenadasActuales)
-                    usuario.setCoorDestino(coordenadaDestino)
-                    usuario.ingresarCoordenadas(coordenadasActuales, coordenadaDestino)
-
-
-                    /*
-                    El usuario después de poner sus coordenadas se le pedirá un método de pago
-                    Los predeterminados son: Efectivo y tarjeta
-                    */
-                    do {
+                if (usuario.loginUsuario(usuario.getUsuario(), usuario.getPassword())) {
+                    if (!usuario.getUsuario().equals("") && !usuario.getPassword().equals("")) {
                         println()
-                        print("Ingrese su método de pago: ")
-                        val pago = readLine()!!.toString()
-                        usuario.setTipoPago(pago)
-                        usuario.metodoDePago(pago)
+                        /*
+                        El usuario después de iniciar sesion tiene que ingresar sus coordenadas
+                        Coordenadas actuales en donde está ubicado actualmente
+                        Coordenadas destino a donde quiere ir
+                         */
+                        print("Ingrese sus coordenadas actuales: ")
+                        var coordenadasActuales = readLine()!!.toInt()
+                        print("Ingrese las coordenadas de su destino: ")
+                        var coordenadaDestino = readLine()!!.toInt()
+                        usuario.setCoorActuales(coordenadasActuales)
+                        usuario.setCoorDestino(coordenadaDestino)
+                        usuario.ingresarCoordenadas(usuario.getCoorActuales(), usuario.getCoorDestino())
+
+                        println()
+                        // Muestra al chofer
+                        val nombre = "Paco"
+                        val coordenadasChofer = 100
+                        println("El nombre de su chofer es: $nombre")
+                        println("se encuentra en la coordenada: $coordenadasChofer")
+                        println()
+                        /*
+                        //Escribe el código a ejecutar
+                        println("El costo de su viaje es: ${calcularCostoViaje(coordenadasActuales, coordenadaDestino)}")
+                        println("El tiempo estimado de su viaje es: ${calcularCostoViaje(coordenadasActuales, coordenadaDestino) * 2}")
+                        tiempoLlegadaChofer(coordenadasChofer, coordenadasActuales)
+
+                         */
+                        break
 
 
-                        when(usuario.getTipoPago()) {
-                            "Efectivo" -> {
-                                println("Agregado Exitosamente el pago con efectivo")
-                                break
-                            }
-                            "Tarjeta" -> {
-                                println("Agregado Exitosamente el pago con tarjeta")
-                                break
-                            }
-                            else -> {
-                                println("METODO DE PAGO INCORRECTO")
-                            }
-                        }
-
-
-
-                    } while (pago.isEmpty() || pago != "Efectivo" || pago != "Tarjeta")
-                    println()
-
-                    // Muestra al chofer
-                    val nombre = "Paco"
-                    val coordenadasChofer = 100
-
-                    println("El nombre de su chofer es: $nombre")
-                    println("se encuentra en la coordenada: $coordenadasChofer")
-                    println()
-
-
-                    //Escribe el código a ejecutar
-                    println("El costo de su viaje es: ${calcularCostoViaje(coordenadasActuales, coordenadaDestino)}")
-                    println("El tiempo estimado de su viaje es: ${calcularCostoViaje(coordenadasActuales, coordenadaDestino) * 2}")
-                    tiempoLlegadaChofer(coordenadasChofer, coordenadasActuales)
-                    
-                    break
-
-
+                    } else
+                        Salidado = false
                 }
-
-                else
-                    println("No ingreso sus datos correctamente")
-                Salidado =false
-
-
-            }
-            2 -> {
-                println()
-                println("Usted selecciono Registro")
-                //registroUsuario(usuario)
             }
 
-            3 -> Salidado=true
-
-
-            else ->{
-                println()
-                println("OPCIÓN INVALIDA!!")
+                3 -> Salidado=true
+                else ->{
+                    println()
+                    println("OPCIÓN INVALIDA!!")
+                }
             }
-        }
-    }while (!Salidado)
 
-
-
+        }while (!Salidado)
 // Impresion
-//println(“$nombreCliente verifica las placas y la foto del conductor”)
-
-}
-
-//Funcion para el inicio de sesion del usuario
-fun LoginUsuario(user: String, password: String, usuario: MutableMap<String,String>): Boolean{
-
-    fun validate(input: String, input2: String):Boolean {
-        if(input.isEmpty() || input2.isEmpty()){
-            return false
-        }
-        else{
-            if(input in usuario ){
-                return  true
-            } else
-                return false
-        }}
-
-    val userValidate = validate(user,password)
-
-    return userValidate
-}
-
-//Funcion para calcular el costo del viaje
-fun calcularCostoViaje(coorActual: Int, coorDestino: Int): Float {
-    var distancia: Int = (coorDestino - coorActual)
-    var precioViaje: Float = (PRECIO_POR_COR * distancia.toFloat()) + PRECIO_BASE
-
-    return precioViaje
-}
-fun tiempoLlegadaChofer(coorChofer: Int, coorUsuario:Int): Float {
-    var diferenciaCoordenadas = coorChofer - coorUsuario
-    var tiempoLlegada: Float = diferenciaCoordenadas.toFloat()
-    val proximidad = diferenciaCoordenadas
-
-    when (proximidad){
-        0 -> println("Su chofer ha llegado")
-        in 1..100 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*2} segundos")
-        in 100..200 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*5.5} segundos")
-        in 200..250 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*8.5} segundos")
-        else -> {
-            println("Su chofer esta muy lejos de su ubicacion")
-        }
+//println("$nombreCliente verifica las placas y la foto del conductor")
     }
-    return proximidad.toFloat()
-}
-fun registroUsuario(usuarios: MutableMap<String, String>){
-    println("Ingrese el nombre de usuario")
-    var Nombre = readLine()!!
-    println("Ingrese su contraseña")
-    var Contrasena = readLine()!!
-    usuarios[Nombre] = Contrasena
-}
+
+    //Funcion para calcular el costo del viaje
+    fun calcularCostoViaje(coorActual: Int, coorDestino: Int): Float {
+        var distancia: Int = (coorDestino - coorActual)
+        var precioViaje: Float = (PRECIO_POR_COR * distancia.toFloat()) + PRECIO_BASE
+        return precioViaje
+    }
+    fun tiempoLlegadaChofer(coorChofer: Int, coorUsuario:Int): Float {
+        var diferenciaCoordenadas = coorChofer - coorUsuario
+        var tiempoLlegada: Float = diferenciaCoordenadas.toFloat()
+        val proximidad = diferenciaCoordenadas
+        when (proximidad){
+            0 -> println("Su chofer ha llegado")
+            in 1..100 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*2} segundos")
+            in 100..200 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*5.5} segundos")
+            in 200..250 -> println("Su Chofer tardara en llegar ${(tiempoLlegada)*8.5} segundos")
+            else -> {
+                println("Su chofer esta muy lejos de su ubicacion")
+            }
+        }
+        return proximidad.toFloat()
+    }
