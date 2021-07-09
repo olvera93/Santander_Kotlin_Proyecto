f638159469aa8d390fd2bd1dd29baef147d47a55
StuartGa <73726431+StuartGa@users.noreply.github.com>
2021-07-09 17:24:18 -0500

    Retos sesion 1

---
+++ src/main/kotlin/s1/Test.kt
@@ -0,0 +1,25 @@
+const val pi = 3.1415
+fun main(){
+    /*println("Ejercicio 1")
+    var n1= .1
+    println(n1.javaClass.kotlin)
+    println("Ejercicio 2 :")
+    var n2=.5f
+    n2.toFloat()
+    println(n2.javaClass.kotlin)
+
+    println("Ejercicio 3 :")
+    val c= pi*(2*(n2))
+    println("El perimetro del circulo es  $c:")
+    */
+//
+    val y2=-2f
+    val y1=3
+    val x1=4
+    val x2=-3f
+    //reto 1
+    var m=(y2-y1)/(x2-x1)
+    println("El valor de la pendiente es : $m")
+
+
+}
